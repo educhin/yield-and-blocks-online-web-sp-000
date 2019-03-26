8265f7d0dda7e8ac29de9faa49708eada5692d96
Chinedu Arinze <c.s.arinze@gmail.com>
2019-03-26 14:15:15 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,16 @@
-def hello_t(arr)
-  i = 0
-  if arr.length > 0
-    while i < arr.length
-      yield arr[i]
-      i += 1
+def hello_t(array)
+  if block_given?
+    i = 0
+
+    while i < array.length
+      yield(array[i])
+      i = i + 1
     end
+
+    array
+  else
+    puts "Hey! No block was given!"
   end
-  arr
 end
 
 # call your method here!
